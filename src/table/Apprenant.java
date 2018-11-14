892ee47af81421ed3485d76f315a9bdb91821cf7
Administrateur <christinebelle.pereira@gmail.com>
2018-11-14 16:47:59 +0100

    Table apprenant et avoir

--- src/table/Apprenant.java
+++ src/table/Apprenant.java
@@ -1,77 +1,121 @@
 package table;
 
+/**
+ * @author Christine
+ * initialisation des variables de la TABLE apprenant
+ * création des constructeurs
+ * création des getter et des setter
+ * 
+ * import de la bibliotheque Date pour gerer le traitement du format date 
+ */
+
+import java.util.Date; // import de la bibliotheque Date
+
 public class Apprenant {
 	
-	/**
-	 * @author Christine
-	 * initialisation des variables de la TABLE activite
-	 * création des constructeurs
-	 * création des getter et des setter
-	 */
-
-	public class Activite {
+		private int apprenantId;
+		private String nom;
+		private String prenom;
+		private Date dateNaissance;
+		private String mail;
+		private String photo;
+		private int regionId;
 		
-		private int activiteId;
-		private String numeroActivite;
-		private String nomActivite;
 		
 		/**
 		 * generation constructeur sans parametre
 		 */
-		public Activite() {
+		public Apprenant() {
 			
-		}
+		}		
 
 		/**
 		 * generation constructeur avec parametre
 		 */
-		public Activite(int activiteId, String numeroActivite, String nomActivite) {
-			
-			this.activiteId = activiteId;
-			this.numeroActivite = numeroActivite;
-			this.nomActivite = nomActivite;
+		public Apprenant(int apprenantId, String nom, String prenom, Date dateNaissance, String mail, String photo,
+				int regionId) {
+			super();
+			this.apprenantId = apprenantId;
+			this.nom = nom;
+			this.prenom = prenom;
+			this.dateNaissance = dateNaissance;
+			this.mail = mail;
+			this.photo = photo;
+			this.regionId = regionId;
 		}
+		
 
 		/**
 		 * generation des getter et des setter
 		 */
-		
-		public int getActiviteId() {
-			return activiteId;
+		public int getApprenantId() {
+			return apprenantId;
 		}
 
-		public void setActiviteId(int activiteId) {
-			this.activiteId = activiteId;
+		public void setApprenantId(int apprenantId) {
+			this.apprenantId = apprenantId;
 		}
 
-		public String getNumeroActivite() {
-			return numeroActivite;
+		public String getNom() {
+			return nom;
 		}
 
-		public void setNumeroActivite(String numeroActivite) {
-			this.numeroActivite = numeroActivite;
+		public void setNom(String nom) {
+			this.nom = nom;
 		}
 
-		public String getNomActivite() {
-			return nomActivite;
+		public String getPrenom() {
+			return prenom;
 		}
 
-		public void setNomActivite(String nomActivite) {
-			this.nomActivite = nomActivite;
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
 		}
-		
+
+		public Date getDateNaissance() {
+			return dateNaissance;
+		}
+
+		public void setDateNaissance(Date dateNaissance) {
+			this.dateNaissance = dateNaissance;
+		}
+
+		public String getMail() {
+			return mail;
+		}
+
+		public void setMail(String mail) {
+			this.mail = mail;
+		}
+
+		public String getPhoto() {
+			return photo;
+		}
+
+		public void setPhoto(String photo) {
+			this.photo = photo;
+		}
+
+		public int getRegionId() {
+			return regionId;
+		}
+
+		public void setRegionId(int regionId) {
+			this.regionId = regionId;
+		}
 
 		/**
 		 * methode toString qui permet l'affichage des variables
 		 */
 		@Override
 		public String toString() {
-			return "la table Activite comprend : activiteId=" + activiteId + ", numeroActivite=" + numeroActivite + ", nomActivite="
-					+ nomActivite ;
+			return "la Table Apprenant comprend : apprenantId=" + apprenantId + ", nom=" + nom + ", prenom=" + prenom + ", dateNaissance="
+					+ dateNaissance + ", mail=" + mail + ", photo=" + photo + ", regionId=" + regionId;
 		}	
 		
-
-	}
-
+		
 
 }
+
+
+
--- src/table/Avoir.java
+++ src/table/Avoir.java
@@ -1,5 +1,59 @@
 package table;
 
+/**
+ * @author Christine
+ * initialisation des variables de la TABLE avoir
+ * création des constructeurs
+ * création des getter et des setter
+ */
+
 public class Avoir {
+
+	private int activiteId;
+	private int apprenantId;
+	
+	
+	/**
+	 * generation constructeur sans parametre
+	 */
+	public Avoir() {
+		
+	}
+
+	/**
+	 * generation constructeur avec parametre
+	 */
+	public Avoir(int activiteId, int apprenantId) {
+		
+		this.activiteId = activiteId;
+		this.apprenantId = apprenantId;
+	}
+
+	/**
+	 * generation des getter et des setter
+	 */
+	public int getActiviteId() {
+		return activiteId;
+	}
+	
+	public void setActiviteId(int activiteId) {
+		this.activiteId = activiteId;
+	}
+	
+	public int getApprenantId() {
+		return apprenantId;
+	}
+	
+	public void setApprenantId(int apprenantId) {
+		this.apprenantId = apprenantId;
+	}
+
+	/**
+	 * methode toString qui permet l'affichage des variables
+	 */
+	@Override
+	public String toString() {
+		return "La Table Avoir comprend : activiteId= " + activiteId + ", apprenantId= " + apprenantId;
+	}	
 
 }
