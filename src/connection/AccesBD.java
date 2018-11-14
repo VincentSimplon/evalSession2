a70769f7630ab2806af8aff462f7e157a5c68664
VincentSimplon <lebeguev@gmail.com>
2018-11-14 16:47:04 +0100

    Connexion à la BD (c'est un succés)

--- src/connection/AccesBD.java
+++ src/connection/AccesBD.java
@@ -7,331 +7,47 @@ package connection;
 ========================================================
  */
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.swing.JOptionPane;
-
-import connection.AccesBD;
 
 public class AccesBD {
+	
+	/** 
+	 * Initialisation des paramètres de connexion pour se connecter à la base de données.
+	 */
 
 
 	private static String utilisateur="root";
 	private static String motDePasse="Bruc3Wayn3023007";
 	private static String pilote = "com.mysql.cj.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost:3306/bd-apprenants?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC\r\n" + 
-			"com.mysql.cj.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost:3306/bd_apprenants?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
-
-	/* 	on declare un objet de la classe Connection de java.sql
-	 * 	Il permet de gerer la connexion entre notre programme java
-	 * 	et la base de donnees.
+	/**
+	 * Création de la méthode connexion pour se connecter à la base de données bd_apprenants.
+	 * Class.forName(url de la BD) permet de charger le pilote.
+	 * Pour initialiser la connexion, il faut créer un objet de type Connection (import connection)qui utilise la méthode getConnection de la classe driveManager pour se connecter.
+	 * Et bien sûr nous gérons les exeptions et on affiche un message en fonction de la réussite ou non de la connexion.
 	 */
-	private static Connection connexion=null;
+	
+	public static void connexion() {
 
-	static
-	{
 		try
 		{
+			// 1 - Charger le pilote.
 			Class.forName(pilote);
-		}
-		catch (Exception e)
-		{
 
-			System.out.println(e);
-			JOptionPane.showMessageDialog(null," non valide ou introuvable !","AccesBD",JOptionPane.WARNING_MESSAGE);
+			// 2 - Initialiser la connexion.
+			Connection connection = DriverManager.getConnection(url, utilisateur, motDePasse);
+			System.out.println("La connexion est établie.");
 
-		}
-	}
-	
-	/**
-	 * Methode qui retourne un objet de type Connection
-	 * @return Connection
-	 */
-	public synchronized static Connection getConnection()
-	{
-		try
-		{
-			if (connexion==null) connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
-
-
-		}catch (Exception e)
-			{
-				System.out.println(e);
-				JOptionPane.showMessageDialog(null,e.getMessage(),"Connexion � MySQL",JOptionPane.WARNING_MESSAGE);
-			}
-
-		return connexion;
-	}
-
-	/**
-	 * m�thode de fermeture d'un objet de type connection
-	 */
-	public static void fermerConnection(Connection connexion)
-	{
-		if (connexion!=null)
-		{
-			try
-			{
-				connexion.close();
-			}
-
-			catch(Exception e)
-			{
-				System.out.println("Fermeture de la Connection Impossible !");
-				e.printStackTrace();
-			}
-		}
-	}
-
-	/**
-	 * m�thode de fermeture d'un objet de type connection
-	 */
-	public static void fermerConnection()
-	{
-		if (connexion!=null)
-		{
-			try
-			{
-				connexion.close();
-			}
-
-			catch(Exception e)
-			{
-				System.out.println("Fermeture de la Connection Impossible !");
-				e.printStackTrace();
-			}
-		}
-	}
-	
-
-	/**
-	 *
-	 * @param unUtilisateur String
-	 */
-	public static void setUtilisateur(String unUtilisateur)
-	{
-		utilisateur=unUtilisateur;
-	}
-	/**
-	 *
-	 * @param unMotDePasse String
-	 */
-	public static void setMotDePasse(String unMotDePasse)
-	{
-		motDePasse=unMotDePasse;
-	}
-	/**
-	 *
-	 * @return un objet connexion
-	 */
-	/*
-	public static Connection getConnexion() {
-		return connexion;
-	}
-	 */
-	/**
-	 *
-	 * @return
-	 */
-	public static String getMotDePasse() {
-		return motDePasse;
-	}
-	/**
-	 *
-	 * @return
-	 */
-	public static String getPilote() {
-		return pilote;
-	}
-	/**
-	 *
-	 * @return
-	 */
-	public static String getUrl() {
-		return url;
-	}
-	/**
-	 *
-	 * @return
-	 */
-	public static String getUtilisateur() {
-		return utilisateur;
-	}
-	/**
-	 *	m�thode d'ex�cution d'une requ�te (SELECT) pour renvoyer un objet de type ResultSet
-	 *	@param requete String
-	 *	@return resultat ResultSet
-	 */
-	public static ResultSet executerQuery(String requete) throws ClassNotFoundException, SQLException
-
-	{
-		/*
-		 * 	On d�clare un objet de type Statement que l'on nomme instruction. Cet
-		 * 	objet soumet la requ�te � la base de donn�es dans MySQL.
-		 * 	On d�clare un objet de type ResultSet que l'on nomme resultat. cet objet
-		 * 	est retourn� pour encapsuler les r�sultats de la requ�te. Il va nous permettre
-		 * 	de manipuler les r�sultats de la requ�te.
-		 *
-		 */
-		Statement statement = null;
-		ResultSet resultat = null;
-
-
-		try {
-
-
-			// gestion des curseurs pour la navigation
-			/*
-			 * TYPE_SROLL_SENSITIVE :
-			 *
-			 *
-			 * TYPE_SCROLL_INSENSITIVE :
-			 * Cette valeur indique que le curseur peut �tre d�plac� dans les deux sens,
-			 * mais aussi arbitrairement (de mani�re absolue ou relative).
-			 * Le terme insensitive indique que le ResultSet est insensible aux modifications
-			 * des valeurs dans la base de donn�es. Cela d�finit en fait une vue statique des donn�es
-			 * contenues dans le ResultSet.
-			 *
-			 * CONCUR_UPDATABLE :
-			 * Cette valeur indique que l'on peut modifier les donn�es de la base via le ResultSet.
-			 */
-
-			int type = ResultSet.TYPE_SCROLL_SENSITIVE;
-			int mode = ResultSet.CONCUR_UPDATABLE;
-
-			/* 	On peut traduire Statement par ordre ou instruction.
-			 * 	La m�thode createStatement() nous retourne un objet de type Statement.
-			 * 	Nous l'avons appel� avec la m�thode getConnection() qui nous renvoie
-			 * 	un objet de type Connexion.
-			 * 	D�s lors, nous pouvons utiliser l'objet instruction pour interroger
-			 * 	la base de donn�es Oracle.
-			 *
-			 */
-			statement = getConnection().createStatement(type,mode);
-			/*	Pour cela, il nous suffit d'appeler la m�thode executeQuery() en lui passant
-			 * 	comme param�tre, la requete que nous voulons ex�cuter.
-			 * 	L'objet resultat contient le r�sultat de l'ex�cution de la requ�te.
-			 */
-
-			resultat = statement.executeQuery(requete);
-
-		}
-
-		catch(SQLException e)
-		{
-
+		} catch(SQLException sqle) {
+			sqle.printStackTrace();
+			System.out.println("La tentative de connexion a echouée.");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			System.exit(-1);
-		}
-		return resultat;	// retourne un ResultSet
-
-
-	}
-
-	/**
-	 *	M�thode d'ex�cution d'une requete Update (UPDATE, INSERT, DELETE). Elle ne renvoie rien
-	 *	@param requete String
-	 */
-	public static void executerUpdate(String requete) throws ClassNotFoundException, SQLException
-
-	{
-		Statement statement = null;
-		try {
-
-			statement = getConnection().createStatement();
-
-			int i = statement.executeUpdate(requete);
-
-
-			if (i==1) // on affiche un message d'information sur l'op�ration pour le plaisir !
-
-			{
-				JOptionPane.showMessageDialog(null, "L'op�ration a r�ussie !");
-			}
-			else
-			{
-				JOptionPane.showMessageDialog(null, "L'op�ration a �chou� !");
-			}
-
-		}
-
-		catch(SQLException e)
-		{
-
-			e.printStackTrace();
-			System.exit(-1);
-		}
-
-	}
-	
-	
-	/**
-	 * m�thode de fermeture d'un objet de type Statement
-	 */
-
-	public static void fermerStatement(Statement statement)
-	{
-		if (statement!=null)
-		{
-			try
-			{
-				statement.close();
-			}
-
-			catch(Exception e)
-			{
-				System.out.println("Fermeture du Statement Impossible !");
-				e.printStackTrace();
-			}
+			System.out.println("La tentative de connexion a echouée.");
 		}
 	}
-
-	/**
-	 * m�thode de fermeture d'un objet de type Statement
-	 */
-
-	public static void fermerResultSet(ResultSet resultSet)
-	{
-		if (resultSet!=null)
-		{
-			try
-			{
-				resultSet.close();
-			}
-
-			catch(Exception e)
-			{
-				System.out.println("Fermeture du ResultSet Impossible !");
-				e.printStackTrace();
-			}
-		}
-	}
-	
-
-	/**
-	 *  test avec m�thode main()
-	*/
-	
-	public static void main(String[] args)
-	{
-		Connection connnect = AccesBD.getConnection();
-		if (connnect!=null)
-		{
-			JOptionPane.showMessageDialog(null, "�a marche");
-		}
-		else
-		{
-			JOptionPane.showMessageDialog(null, "�a marche PAS !");
-		}
-
-	}
-
-
-
-
 }
--- src/main/Affichage.java
+++ src/main/Affichage.java
@@ -1,5 +1,12 @@
 package main;
 
+import connection.AccesBD;
+
 public class Affichage {
+	
+	public static void main(String[] args) {
+		
+		AccesBD.connexion();
+	}
 
 }
