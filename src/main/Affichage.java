245607279a0d597e380f2792beaf77fb0eb53564
VincentSimplon <lebeguev@gmail.com>
2018-11-15 15:27:14 +0100

    un commit pour une biere

--- src/main/Affichage.java
+++ src/main/Affichage.java
@@ -1,6 +1,6 @@
 package main;
 
-
+import java.util.Scanner;
 
 import java.sql.SQLException;
 
@@ -8,13 +8,57 @@ import methodesRequetes.Requetes;
 
 public class Affichage {
 	
+	static Scanner saisie;
+	
 	public static void main(String[] args) throws SQLException {
 		
-		Requetes.afficherNomEtPrenomTousLesApprenants();
-		Requetes.listeApprenantsPourChaqueRegion();
-		Requetes.rechercheApprenantAfficheActivite();
-		Requetes.rechercheApprenantPratiqueActivite("005");
+		int choixUtilisateur;
+		do {
+		saisie = new Scanner(System.in);
+		System.out.println("bla");
 		
+		
+		
+		System.out.println("==========================================================\n" + 
+		"Bienvenue sur la base de données des apprenants Simplon 3.\n" + 
+		"==========================================================\n" + 
+		"Que souhaitez vous faire ?\n" + " \n" + 
+		"1 - Afficher le nom et le prénom de tous les apprenants.\n" + 
+		"2 - Afficher la liste des apprenants pour chaque région.\n" + 
+		"3 - Afficher les acivités d'un apprenant spécifique.\n" + 
+		"4 - Rechercher des apprenants qui effectuent une activité spécifique.\n" + 
+		"5 - Quitter. \n" + 
+		"Veuillez saisir votre choix : \n");
+		
+		choixUtilisateur = saisie.nextInt();
+		
+		switch (choixUtilisateur) {
+		
+		case 1 :
+			Requetes.afficherNomEtPrenomTousLesApprenants();
+			break;
+			
+		case 2 : 
+			Requetes.listeApprenantsPourChaqueRegion();
+			break;
+			
+		case 3 : 
+			Requetes.rechercheApprenantAfficheActivite();
+			break;
+			
+		case 4 : 
+			Requetes.rechercheApprenantPratiqueActivite("");
+			break;
+			
+		case 5 : 
+			System.out.println("A bientôt");
+			break;
+			
+		default :
+			System.out.println("Veuillez saisir un choix entre 1 et 5.");
+		}
+		
+		} while (choixUtilisateur != 5);
 		
 	}
 }
--- src/methodesRequetes/Requetes.java
+++ src/methodesRequetes/Requetes.java
@@ -11,34 +11,48 @@ import table.Activite;
 import table.Apprenant;
 
 public class Requetes {
-	
+	 
 	static Scanner saisie;
 
 	public static void afficherNomEtPrenomTousLesApprenants() throws SQLException {
 
-		ResultSet resultat = AccesBd.faireUneRequete("SELECT nom, prenom FROM apprenant");
-
+		ResultSet resultat = AccesBd.faireUneRequete("SELECT apprenantId, nom, prenom FROM apprenant");
+		
+		System.out.println("===================================");
+ 
 		while(resultat.next()) {
-
-			String colonne1 = resultat.getString("nom");
-			String colonne2 = resultat.getString("prenom");
-			System.out.println(colonne1 + " " + colonne2);
+			int colonne1 = resultat.getInt("apprenantId");
+			String colonne2 = resultat.getString("nom");
+			String colonne3 = resultat.getString("prenom");
+			System.out.println("-" + colonne1 + " " + colonne2 + " " + colonne3);
 
 		}
+		
+		System.out.println("===================================");
 	}
 	
 	public static void listeApprenantsPourChaqueRegion() throws SQLException {
 		
-		ResultSet resultat = AccesBd.faireUneRequete("SELECT apprenant.nom, apprenant.prenom, region.nom FROM apprenant, region WHERE apprenant.regionId = region.regionId");
+		saisie = new Scanner(System.in);
+		System.out.println("Pour quelle région souhaitez vous avoir la liste des apprenants ?");
+		String region = saisie.nextLine();
+		
+		PreparedStatement prepaInstru = AccesBd.getConnection().prepareStatement("SELECT apprenant.nom, apprenant.prenom, region.nom FROM apprenant, region WHERE apprenant.regionId = region.regionId AND region.nom = ?");
+		prepaInstru.setString(1, region);
+		ResultSet resultat = prepaInstru.executeQuery();
+		
+		System.out.println("===================================");
 		
 		while(resultat.next()) {
 			
 			String colonne1 = resultat.getString("apprenant.nom");
 			String colonne2 = resultat.getString("apprenant.prenom");
 			String colonne3 = resultat.getString("region.nom");
-			System.out.println(colonne1 + " " + colonne2 + " " + colonne3);
+			System.out.println(colonne1 + " " + colonne2 + " : " + colonne3);
 			
 		}
+		
+		System.out.println("===================================");
 	}
 	
 	public static void rechercheApprenantAfficheActivite() throws SQLException {
@@ -47,33 +61,45 @@ public class Requetes {
 		System.out.println("Entrez le nom d'un apprenant pour afficher ses activités : ");
 		String apprenant = saisie.nextLine();
 		
-		PreparedStatement prepaInstru = AccesBd.getConnection().prepareStatement("SELECT avoir.activiteId, activite.typeActivite, apprenant.nom FROM avoir, activite, apprenant WHERE activite.activiteId = avoir.activiteId AND apprenant.apprenantID = avoir.apprenantId AND apprenant.nom = ?");
+		PreparedStatement prepaInstru = AccesBd.getConnection().prepareStatement("SELECT avoir.activiteId, activite.nomActivite, apprenant.nom FROM avoir, activite, apprenant WHERE activite.activiteId = avoir.activiteId AND apprenant.apprenantID = avoir.apprenantId AND apprenant.nom = ?");
 		prepaInstru.setString(1, apprenant);
 		ResultSet resultat = prepaInstru.executeQuery();
+		
+		System.out.println("===================================");
+		
 		while(resultat.next()) {
 			
 			String colonne1 = resultat.getString("avoir.activiteId");
-			String colonne2 = resultat.getString("activite.typeActivite");
+			String colonne2 = resultat.getString("activite.nomActivite");
 			String colonne3 = resultat.getString("apprenant.nom");
 			System.out.println(colonne1 + " " + colonne2 + " " + colonne3);
 		}
 		
+		System.out.println("===================================");
 		
 	}
 	
 	public static void rechercheApprenantPratiqueActivite(String activite) throws SQLException {
 		
+		saisie = new Scanner(System.in);
+		System.out.println("Pour quelle activité souhaitez vous afficher les apprenants ?");
+		activite = saisie.nextLine();
 		
-		PreparedStatement prepaInstru = AccesBd.getConnection().prepareStatement("SELECT apprenant.nom, activite.typeActivite FROM avoir, activite, apprenant WHERE activite.activiteId = avoir.activiteId AND apprenant.apprenantID = avoir.apprenantId AND activite.numeroActivite = ?");
+		PreparedStatement prepaInstru = AccesBd.getConnection().prepareStatement("SELECT apprenant.nom, activite.nomActivite FROM avoir, activite, apprenant WHERE activite.activiteId = avoir.activiteId AND apprenant.apprenantID = avoir.apprenantId AND activite.numeroActivite = ?");
 		prepaInstru.setString(1, activite);
 		ResultSet resultat = prepaInstru.executeQuery();
+		
+		System.out.println("===================================");
+		
 		while(resultat.next()) {
 			
 			String colonne1 = resultat.getString("apprenant.nom");
-			String colonne2 = resultat.getString("activite.typeActivite");
+			String colonne2 = resultat.getString("activite.nomActivite");
 			System.out.println(colonne1 + " " + colonne2);
 	}
 
+		System.out.println("===================================");
+		
 	}
 }
 
