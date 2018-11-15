70d4d853ca1dfdd9ed2229151a1ca41931ff30dd
VincentSimplon <lebeguev@gmail.com>
2018-11-15 16:46:08 +0100

    Tous les tests fonctionnent ! C'est chouette ! Nous sommes vachement balaise !

--- src/main/TestCrud.java
+++ src/main/TestCrud.java
@@ -6,15 +6,23 @@ import java.sql.SQLException;
 
 import methodesRequetes.Crud;
 import table.Apprenant;
+import table.Avoir;
 
 public class TestCrud {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		Apprenant Bruce = new Apprenant("Wayne", "Vincent", null, "batman@gotham.fr", " ", 1);
-		Crud.createNewApprenant(Bruce);
+		Crud.updateName();
 		
-		Crud.activiteNotUse();
+		//Avoir ajoutActivite = new Avoir(8, 17);
+		//Crud.newActiviteNewApprenant(ajoutActivite);
+		
+		//Crud.deleteApprenant();
+		
+		//Apprenant Bruce = new Apprenant("Wayne", "Vincent", null, "batman@gotham.fr", " ", 1);
+		//Crud.createNewApprenant(Bruce);
+		
+		//Crud.activiteNotUse();
 		
 	}
 
